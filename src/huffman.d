02702e30f3be09d07b7dc3cdf6cffaa50026164f
Anthony Wang <ta180m@gmail.com>
2020-05-25 01:46:09 +0000

    Added Makefile

---
+++ src/huffman.d
@@ -0,0 +1,5 @@
+src/huffman.o: src/huffman.cpp src/huffman.h src/common.h
+
+src/huffman.h:
+
+src/common.h:
---
+++ src/main.d
@@ -0,0 +1,5 @@
+src/main.o: src/main.cpp src/common.h src/huffman.h
+
+src/common.h:
+
+src/huffman.h:
